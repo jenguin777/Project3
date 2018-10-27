8f76620e2f673e1d6743e372ee9b1473cf210dfd
mwkrier <michaelwkrier@gmail.com>
2018-10-27 12:38:52 -0400

    ingredients add to html

--- client/src/pages/Ingredients/Ingredients.js
+++ client/src/pages/Ingredients/Ingredients.js
@@ -4,14 +4,11 @@ import API from "../../utils/API";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
-import { Input, TextArea, FormBtn } from "../../components/Form";
+import { Input, TextArea, FormBtn} from "../../components/Form";
 
 class Ingredients extends Component {
   state = {
-    ingredients: [],
-    title: "",
-    author: "",
-    synopsis: ""
+    name: "" ,
   };
 
   componentDidMount() {
@@ -21,7 +18,7 @@ class Ingredients extends Component {
   loadIngredients = () => {
     API.getIngredients()
       .then(res =>
-        this.setState({ ingredients: res.data, title: "", author: "", synopsis: "" })
+        this.setState({ ingredients: res.data, name: ""})
       )
       .catch(err => console.log(err));
   };
@@ -41,9 +38,10 @@ class Ingredients extends Component {
 
   handleFormSubmit = event => {
     event.preventDefault();
-    if (this.state.ingredient) {
+
+    if (this.state.name) {
       API.saveIngredient({
-        ingredient: this.state.ingredient
+        name: this.state.name
       })
         .then(res => this.loadIngredients())
         .catch(err => console.log(err));
@@ -59,18 +57,38 @@ class Ingredients extends Component {
               <h1>What Ingredients Do I Have?</h1>
               <form>
                 <Input
-                  value={this.state.ingredient}
+                  value={this.state.name}
                   onChange={this.handleInputChange}
-                  name="ingredient"
+                  name="name"
                   placeholder="Ingredient (required)"
                 />
                 <FormBtn
-                  disabled={!(this.state.ingredient)}
+                  disabled={!(this.state.name)}
                   onClick={this.handleFormSubmit}
                 >
                   Submit Ingredient
                 </FormBtn>
               </form>
+              
+              {this.state.ingredients ? (
+              <List>
+                {this.state.ingredients.map(ingredients => {
+                  return (
+                    <ListItem key={ingredients._id}>
+                      <a href={"/ingredients/" + ingredients._id}>
+                        <strong>
+                          {ingredients.name}
+                        </strong>
+                      </a>
+                      <DeleteBtn onClick={() => this.deleteIngredient(ingredients._id)} />
+                    </ListItem>
+                  );
+                })}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+          
             </div>
           </Col>
           <Col size="md-6 sm-12">
