59d82101f640ac79f714e303aab8f35f253529e5
unknown <38478896+nlee728@users.noreply.github.com>
2018-11-03 12:36:57 -0400

    new branch with everything working

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,10 +9,10 @@ router.use('/auth', authRoutes);
 // API Routes
 router.use("/api", apiRoutes);
 
-// If no routes are hit, send the React app
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+// // If no routes are hit, send the React app
+// router.use(function(req, res) {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 
 module.exports = router;
